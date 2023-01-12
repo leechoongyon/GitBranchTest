a13671142a835ed68cff66c4264db198ee04ce43
LeeJunGyun <rainmaker0303@hanmail.net>
2023-01-12 13:18:47 +0000

    codespace test

--- src/main/java/develop/Develop1.java
+++ src/main/java/develop/Develop1.java
@@ -1,4 +1,9 @@
 package develop;
 
 public class Develop1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
 }
