10d1c41e9f2fa7225f252ca3637f6fdb286aa049
LeeJunGyun <rainmaker0303@hanmail.net>
2023-01-12 22:45:01 +0900

    test

--- src/main/java/develop/Develop1.java
+++ src/main/java/develop/Develop1.java
@@ -6,38 +6,5 @@ public class Develop1 {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-<<<<<<< HEAD
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-=======
->>>>>>> 3aa15696d841d5453eb90f803026a46dc57d7550
     }
 }
--- src/main/java/develop/Develop2.java
+++ src/main/java/develop/Develop2.java
@@ -2,13 +2,8 @@ package develop;
 
 public class Develop2 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("HelloWorld Develop2...");
-=======
         System.out.println("Develop2.....");System.out.println("Develop2.....");
         System.out.println("Develop2.....");
         System.out.println("Develop2.....");System.out.println("Develop2.....");
-        
->>>>>>> 3aa15696d841d5453eb90f803026a46dc57d7550
     }
 }
