dbf3f89fde54b0a352b2ce36735d0624d1da0169
LeeJunGyun <rainmaker0303@hanmail.net>
2023-01-12 13:32:56 +0000

    dfsdf

--- src/main/java/develop/Develop1.java
+++ src/main/java/develop/Develop1.java
@@ -27,5 +27,13 @@ public class Develop1 {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
