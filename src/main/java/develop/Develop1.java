acc6064bdf18e54d0ef52e660a6c7fe44b1e7736
LeeJunGyun <rainmaker0303@hanmail.net>
2023-01-12 22:53:10 +0900

    test

--- src/main/java/develop/Develop1.java
+++ src/main/java/develop/Develop1.java
@@ -3,8 +3,12 @@ package develop;
 public class Develop1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
+        System.out.println("Develop1");
     }
 }
