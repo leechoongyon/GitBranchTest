3aa15696d841d5453eb90f803026a46dc57d7550
LeeJunGyun <rainmaker0303@hanmail.net>
2023-01-12 13:40:37 +0000

    tewtstr

--- src/main/java/develop/Develop2.java
+++ src/main/java/develop/Develop2.java
@@ -2,6 +2,9 @@ package develop;
 
 public class Develop2 {
     public static void main(String[] args) {
+        System.out.println("Develop2.....");System.out.println("Develop2.....");
         System.out.println("Develop2.....");
+        System.out.println("Develop2.....");System.out.println("Develop2.....");
+        
     }
 }
